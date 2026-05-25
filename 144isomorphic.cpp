7123f57973fc17df18fa41fcd6a4599133ecdc14
ak011125 <ajaykumarjakhar08@gmail.com>
2026-05-25 15:26:16 +0530

    question on strings

--- 144isomorphic.cpp
+++ 144isomorphic.cpp
@@ -9,10 +9,21 @@ bool isIsomorphic(string s1, string s2){
     vector<int> v1(128,-1);
     vector<int> v2(128,-1);
 
-    
+    if(s1.size() != s2.size()){ //if strings length does not match
+        return false;
+    }
+
+    for(int i = 0; i<s1.size(); i++){
+        if(v1[s1[i]] != v2[s2[i]]){ // check if value of char st idx i match in both v1 and v2
+            return false;
+        }
+        v1[s1[i]] = v2[s2[i]] = i; // storing string position in vectors for char at idx i 
+    }
+    return true;
 }
 int main(){
     string s1, s2;
+    cout<<"Enter the strings : ";
     cin>>s1>>s2;
 
     if(isIsomorphic(s1,s2)){
@@ -22,3 +33,5 @@ int main(){
     }
     return 0;
 }
+//Time complexity = O(length of strings)
+//space complexity = O(256) constant 
