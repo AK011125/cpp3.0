1d52fd80f0048df7a5d4036db8abcc0ea6bf3be8
ak011125 <ajaykumarjakhar08@gmail.com>
2026-05-30 13:22:22 +0530

    Sring question alternative

--- 145LongestCommoPrefix.cpp
+++ 145LongestCommoPrefix.cpp
@@ -25,6 +25,25 @@ string longestCommonPrefix(vector<string> &str){
     return ans;
 }
 
+string longestCommonPrefix2(vector<string> &str){
+
+    //keeping first string as const and comparing it with all other strings
+    string s1 = str[0];
+    int ans_length = s1.size();
+
+    for(int i=1; i<str.size(); i++){
+
+        int j = 0;
+        while(j<s1.size() && j<str[i].size() && s1[j] == str[i][j]){ //finding the common prefix string length
+            j++;
+        }
+        ans_length = min(ans_length,j); //updating the length if ans string
+
+        string ans = s1.substr(0, ans_length);
+        return ans;
+    }
+}
+
 int main(){
     int n;
     cout<<"Enter no of strings : ";
@@ -37,5 +56,12 @@ int main(){
     }
 
     cout<<"Longest common prefix : "<<longestCommonPrefix(str)<<endl;
+    cout<<"Longest common prefix : "<<longestCommonPrefix2(str)<<endl;
     return 0;
 }
+//Time complexity ->> Time of sorting + Time of while loop
+// O(nlogn*m) where n is no of strings and general length of strings
+// O(min(s[0].size(),s[n-1].size()))
+
+
+// in case 2  time complexity is O(n-1*m) n is no of string and m is general len of str
