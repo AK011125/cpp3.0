420d8c997d245106d4b944a3617bd960635218fd
ak011125 <ajaykumarjakhar08@gmail.com>
2026-06-09 16:40:25 +0530

    Delete Duplicate

---
+++ 162ProblemDeleteDuplicate.cpp
@@ -0,0 +1,10 @@
+//Given the head of a sorted linked list, delete all duplicates such that each element appears only once. return the linked list sorted as well
+#include<iostream>
+using namespace std;
+
+
+
+int main(){
+
+    return 0;
+}
